4f1d1d2f1eb9ae46e887586f9266706452fd6b36
Jeff M <jkdmarks@gmail.com>
2019-03-11 19:48:15 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  output_array = []
+  for x in array
+    if yield x
+      output_array.push(x)
+    else
+      nil
+    end
+  end
+  output_array
 end
